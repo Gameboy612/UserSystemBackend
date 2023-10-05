55bad6be5435375ee38338a356530cbe9427bc65
Gameboy612 <lokapakki@gmail.com>
2023-10-06 01:36:05 +0800

    example return type for aa

--- methods/user/login/password/secure_password.py
+++ methods/user/login/password/secure_password.py
@@ -1,4 +1,18 @@
+'''
+Example Return Type:
 
+return {
+    "success": False,
+    "response": "Password less than * characters.",
+    "data": {}
+}
+
+return {
+    "success": True,
+    "response": "Password available.",
+    "data": {}
+}
+'''
 
 # AA
 def check_security(password: str) -> dict:
