34ed89ee75c3d955feb0a290218257e5d10b18c4
DoubleA <daa0429xy@gmail.com>
2023-10-06 02:11:19 +0800

    check characters

--- methods/user/login/password/secure_password.py
+++ methods/user/login/password/secure_password.py
@@ -1,19 +1,30 @@
-'''
-Example Return Type:
+# '''
+# Example Return Type:
 
-return {
-    "success": False,
-    "response": "Password less than * characters.",
-    "data": {}
-}
+# return {
+#     "success": False,
+#     "response": "Password less than * characters.",
+#     "data": {}
+# }
 
-return {
-    "success": True,
-    "response": "Password available.",
-    "data": {}
-}
-'''
+# return {
+#     "success": True,
+#     "response": "Password available.",
+#     "data": {}
+# }
+
 
 # AA
 def check_security(password: str) -> dict:
-    ...
+    if len(password) >= 8:
+        return {
+     "success": True,
+     "response": "Password available.",
+     "data": {}
+      }
+    else:
+         return {
+     "success": False,
+     "response": "Password less than 8 characters.",
+     "data": {}
+      }
