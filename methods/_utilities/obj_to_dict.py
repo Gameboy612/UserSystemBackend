1f33e2587050b7260050e3ff7082034ace4902e1
Gameboy612 <lokapakki@gmail.com>
2023-10-26 14:08:04 +0800

    obj_to_dict documentation

--- methods/_utilities/obj_to_dict.py
+++ methods/_utilities/obj_to_dict.py
@@ -3,7 +3,15 @@
 from sqlalchemy.orm import class_mapper
 
 def obj_to_dict(obj):
-    """Converts a SQLAlchemy object to a dictionary."""
+    """Converts a SQLAlchemy object to a dictionary.
+
+    Args:
+        obj (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
     mapper = class_mapper(obj.__class__)
     attributes = [attr.key for attr in mapper.iterate_properties if hasattr(obj, attr.key)]
     return {a: getattr(obj, a) for a in attributes}
